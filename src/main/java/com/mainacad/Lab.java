8e58e5b95ad168793eb4cffec0c7b91a88b6fb4c
obilyk <oleg.bilyk@onseo.biz>
2019-02-18 17:28:18 +0200

    Initial commit

---
+++ src/main/java/com/mainacad/Lab.java
@@ -0,0 +1,45 @@
+package com.mainacad;
+
+/**
+ * Hello world!
+ *
+ */
+public class Lab
+{
+    public static void main( String[] args )
+    {
+
+        //TODO Открыть тестовую страницу: https://www.toolsqa.com/automation-practice-form
+
+        //TODO: Вывести в консоль текст Инфо сообщения
+
+        //TODO: Вывести в консоль текст заголовка формы ("Practice Automation Form")
+
+        //TODO: Кликнуть линк Partial Link Test
+
+        //TODO: Заполнить поле First name:
+
+        //TODO: Заполнить поле Last name:
+
+        //TODO: Выбрать пол
+
+        //TODO: Выбрать количество лет опыта
+
+        //TODO: Заполнить поле дата
+
+        //TODO: Выбрать несколько Automation Tool
+
+        //TODO: Выбрать континент из выпадающего списка
+
+        //TODO: Выбрать несколько вариантов из списка Selenium Commands
+
+        //TODO: Кликнуть на кнопку Button
+
+        //TODO: Вывести в консоль текст лейбла Text1
+
+        //TODO: Вывести в консоль текст лейбла Text2
+
+        //TODO: Закрыть браузер
+
+    }
+}
